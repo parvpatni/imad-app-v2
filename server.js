1eb3b605ae7fe5cdf4abe08c9a8e7081bc9991a8
Parv Patni <parvpatni@gmail.com>
2017-02-21 23:39:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/', function (req, res) {
 
 var counter=0;
 app.get('/counter', function(req, res) {
-    counter+=counter;
+    counter=counter+1;
     res.send(counter.toString());
 });
 
