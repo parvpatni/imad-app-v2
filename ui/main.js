ab85f737be80ad1ee62d5b7419d4ad3a577c2028
Parv Patni <parvpatni@gmail.com>
2017-02-22 00:06:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,20 @@ var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function()
 {
-    counter=counter+1;
-var span =document.getElementById('count');
+    var requset= new XMLHttpRequest();
+    requset.onreadystatechange=function()
+    {
+        if(request.readystate===XMLHttpRequest.DONE)
+    {
+        if(request.status===200)
+        {
+            var counter=request.respopnseText;
+            var span =document.getElementById('count');
 span.innerHTML=counter.toString();
+        }
+    }
+    }
+    request.open('GET','http://parvpatni.imad.hasura-app.io/counter',true);
+    request.send(null);
+
 };
